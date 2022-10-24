63a3dbc55d2ddab93ecf82e406517c955250c451
frankshicar <93965977+frankshicar@users.noreply.github.com>
2022-10-24 18:07:06 +0800

    new

---
+++ index.js
@@ -0,0 +1,12 @@
+// $( document ).ready(function() {
+    
+//     $("#animatebutton").click(function(){
+//         const element =  document.querySelector('.animatebutton');
+//         element.classList.add('animated', 'hinge');
+//         setTimeout(function() {
+//           element.classList.remove('hinge'); 
+//   },        1000); 
+//     });
+      
+     
+// });   
